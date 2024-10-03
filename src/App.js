c6bf6da0a8fecfe02d0d63f39071a147b5146f00
Rajapandi98 <rajapandi2930@gmail.com>
2024-10-03 12:23:48 +0530

    form-validation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function App(props) {
+
+  const[usersList,setUsersList]= useState([])
+
+  function addUserHandler(uname,uAge) {
+    setUsersList((prevUserList) =>{
+      return[...prevUserList, {name: uname, age: uAge, id: Math.random().toString()}]
+    });
+  }
+
+  return ( 
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </div> 
   );
 }
 
